d4003729f14a0cb971586281fc187f259823ce13
David Swanson <54049277+mushBrainDave@users.noreply.github.com>
2021-05-17 21:50:53 +0000

    Starting card class file

--- Blackjack/Blackjack/Program.cs
+++ Blackjack/Blackjack/Program.cs
@@ -4,7 +4,7 @@ namespace Blackjack
 {
     class Program
     {
-        static Random cardRandomizer = new Random();
+        static readonly Random cardRandomizer = new Random();
 
         static readonly Card[] playerCards = new Card[11];
         static int playerTotal = 0;
@@ -51,7 +51,7 @@ namespace Blackjack
                     Console.WriteLine("Would you like to (H)it or (S)tay?");
                     playerChoice = Console.ReadLine().ToUpper();
                 }
-                while (!playerChoice.Equals("H") && !playerChoice.Equals("H"));
+                while (!playerChoice.Equals("H") && !playerChoice.Equals("S"));
 
                 if (playerChoice.Equals("H"))
                 {
@@ -110,7 +110,7 @@ namespace Blackjack
             {
                 do
                 {
-                    Console.WriteLine("Would you like to hit or stay? h for hit s for stay");
+                    Console.WriteLine("Would you like to (H)it or (S)tay?");
                     playerChoice = Console.ReadLine().ToUpper();
                 }
                 while (!playerChoice.Equals("H") && !playerChoice.Equals("S"));
@@ -119,42 +119,6 @@ namespace Blackjack
                     Hit();
                 }
             }
-        }
-
-        //TODO: Move this class to it's own file.
-        private class Card
-        {
-            public int Value;
-            public string Name;
-        }
-
-        static Card DealCard()
-        {
-            int cardValue = cardRandomizer.Next(1, 14);
-            playerTotal += cardValue;
-            return GetCardValue(cardValue);
-        }
-
-
-        static Card GetCardValue(int cardValue)
-        {
-            return cardValue switch
-            {
-                1 => new Card() { Name = "Two", Value = 2 },
-                2 => new Card() { Name = "Three", Value = 3 },
-                3 => new Card() { Name = "Four", Value = 4 },
-                4 => new Card() { Name = "Five", Value = 5 },
-                5 => new Card() { Name = "Six", Value = 6 },
-                6 => new Card() { Name = "Seven", Value = 7 },
-                7 => new Card() { Name = "Eight", Value = 8 },
-                8 => new Card() { Name = "Nine", Value = 9 },
-                9 => new Card() { Name = "Ten", Value = 10 },
-                10 => new Card() { Name = "Jack", Value = 10 },
-                11 => new Card() { Name = "Queen", Value = 10 },
-                12 => new Card() { Name = "King", Value = 10 },
-                13 => new Card() { Name = "Ace", Value = 11 },
-                _ => new Card() { Name = "Two", Value = 2 },
-            };
         }
 
         static void PlayAgain()
@@ -177,16 +141,8 @@ namespace Blackjack
             }
             else if (playAgain.Equals("N"))
             {
-                ConsoleKeyInfo info = Console.ReadKey();
-                if (info.Key == ConsoleKey.Enter)
-                {
-                    Environment.Exit(0);
-                }
-                else
-                {
-                    Console.Read();
-                    Environment.Exit(0);
-                }
+                Console.WriteLine("Thanks for playing BT Blackjack!");
+                Environment.Exit(0);
             }
         }
     }
