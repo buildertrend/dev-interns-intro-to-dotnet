656f5a59c72452ef74f8cbaca6cfa172de597009
mushBrainDave <dswanson01@unomaha.edu>
2021-05-18 08:26:28 -0500

    Idea for separating out user interactions

---
+++ Blackjack/Blackjack/Prompts.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Blackjack
+{
+    /// <summary>
+    /// Contains command line output prompting players for the next action.
+    /// </summary>
+    public class prompts
+    {
+        static string playAgain = "Y";
+
+        /*public Player WelcomePlayer()
+        {
+            while (playAgain.ToUpper() == "Y")
+            {
+                //StartGame
+                Console.WriteLine("Welcome to Blackjack - are you ready to play? (Y)es (N)o");
+                var decision = Console.ReadLine().ToUpper();
+            }
+        }*/
+    }
+}
