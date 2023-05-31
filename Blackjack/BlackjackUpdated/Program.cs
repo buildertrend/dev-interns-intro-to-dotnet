1d0b22d32876e3c1b85cbf940e91d8a232b0c776
Nick Vazquez <nick.vazquez@buildertrend.com>
2023-05-31 15:57:31 -0500

    Integrate multiplayer into base program.

--- Blackjack/BlackjackUpdated/Program.cs
+++ Blackjack/BlackjackUpdated/Program.cs
@@ -7,17 +7,15 @@ namespace BlackjackUpdated
 {
     partial class Program
     {
+        static int numPlayers = 0;
+        static Player[]? players = null; 
         static Deck deck = new Deck();
 
-        static readonly Card[] playerCards = new Card[11];
-        static int playerTotal = 0;
-        static int playerCardCount = 1;
         private static readonly Card[] dealerCards = new Card[11];
         static int dealerTotal = 0;
 
         //users to store the player choice (hit or stay)
         static string playerChoice = "";
-
         static string playAgain = "Y";
 
         static void Main(string[] args)
@@ -39,20 +37,34 @@ namespace BlackjackUpdated
 
                 if (decision == "Y")
                 {
+                    // Identify the number of players in the game
+                    bool converted = false;
+                    while (!converted)
+                    {
+                        Console.WriteLine("Enter the number of players to include in the game");
+                        string input = Console.ReadLine();
+                        converted = int.TryParse(input, out numPlayers);
+                    }
+                    players = new Player[numPlayers];
+
+                    // Deal to the dealer first
                     dealerCards[0] = deck.draw();
                     dealerCards[1] = deck.draw();
 
                     dealerTotal += dealerCards[0].Value;
                     dealerTotal += dealerCards[1].Value;
 
-                    playerCards[0] = deck.draw();
-                    playerCards[1] = deck.draw();
-
-                    playerTotal += playerCards[0].Value;
-                    playerTotal += playerCards[1].Value;
-
-
-                    //TODO: The dealer is dealt one card face up, one card face down.
+                    // Deal to each one of the players
+                    for (int i = 0; i < numPlayers; i++)
+                    {
+                        Player player = new Player();
+                        player.cards[0] = deck.draw();
+                        player.cards[1] = deck.draw();
+                        player.total += player.cards[0].Value;
+                        player.total += player.cards[1].Value;
+                        players[i] = player;
+                    }
+                    
                     DisplayWelcomeMessage();
                 }
                 else
@@ -61,31 +73,41 @@ namespace BlackjackUpdated
                 }
 
                 /* START GAME LOOP */
-                do
+                for (int playerNum = 0; playerNum < numPlayers; playerNum++)
                 {
-                    Console.WriteLine("Would you like to (H)it or (S)tay?");
-                    playerChoice = Console.ReadLine().ToUpper();
-                }
-                while (!playerChoice.Equals("H") && !playerChoice.Equals("S"));
+                    Player currentPlayer = players[playerNum];
+                    if (currentPlayer.busted) { continue; }
 
-                if (playerChoice.Equals("H"))
-                {
-                    //hit will get them a card / check the total and ask for another hit
-                    Hit();
-                }
+                    Console.WriteLine();
+                    Console.WriteLine("Current turn: Player {0}", playerNum + 1);
+                    DisplayPlayerCards(currentPlayer);
 
-                if (playerChoice.Equals("S"))
-                {
-                    if (playerTotal > dealerTotal && playerTotal <= 21)
+                    do
                     {
-                        Console.WriteLine("Congrats! You won the game! The dealer's total is {0} ", dealerTotal);
+                        Console.WriteLine("Would you like to (H)it or (S)tay?");
+                        playerChoice = Console.ReadLine().ToUpper();
                     }
-                    else if (playerTotal < dealerTotal)
+                    while (!playerChoice.Equals("H") && !playerChoice.Equals("S"));
+
+                    if (playerChoice.Equals("H"))
                     {
-                        Console.WriteLine("Sorry, you lost! The dealer's total was {0}", dealerTotal);
+                        //hit will get them a card / check the total and ask for another hit
+                        Hit(currentPlayer);
+                    }
+
+                    // TODO: Only display this once all players have either stayed or busted.
+                    if (playerChoice.Equals("S"))
+                    {
+                        if (currentPlayer.total > dealerTotal && currentPlayer.total <= 21)
+                        {
+                            Console.WriteLine("Congrats! You won the game! The dealer's total is {0} ", dealerTotal);
+                        }
+                        else if (currentPlayer.total < dealerTotal)
+                        {
+                            Console.WriteLine("Sorry, you lost! The dealer's total was {0}", dealerTotal);
+                        }
                     }
                 }
-
                 /* END GAME LOOP */
 
                 PlayAgain();
@@ -98,62 +120,65 @@ namespace BlackjackUpdated
         private static void DisplayWelcomeMessage()
         {
             Console.WriteLine("The dealer's first card is: {0}.", dealerCards[0].Name);
-            Console.WriteLine("You were dealt the cards : {0} and {1} ", playerCards[0].Name, playerCards[1].Name);
-            Console.WriteLine("Your player total is {0} ", playerTotal);
-            //TODO: Inform the player the value of the dealer's visible card.
         }
 
-        // <summary>
-        // Checks if there are ny aces in the hand. These are automatically set to a value of 11 (hard ace),
-        // but can be set to a 1 to prevent the player from busting.
-        // </summary>
-        private static void AlterAces()
+        private static void DisplayPlayerCards(Player player)
+        {
+            Console.WriteLine("You were dealt the cards : {0} and {1} ", player.cards[0].Name, player.cards[1].Name);
+            Console.WriteLine("Your player total is {0} ", player.total);
+        }
+
+        /// <summary>
+        /// Checks if there are any aces in the hand. These are automatically set to a value of 11 (hard ace),
+        /// but can be set to a 1 to prevent the player from busting.
+        /// </summary>
+        private static void AlterAces(Player player)
         {
             bool alteredFlag = false;
-
-            playerTotal = 0;
-            for (int i = 0; i < playerCards.Length; i++)
+            int currPlayerTotal = 0;
+            
+            for (int i = 0; i < player.cards.Length; i++)
             {
-                if (playerCards[i] == null) { continue; }
-                if (playerCards[i].Equals(new Card() { Name = "Ace", Value = 11 }))
+                if (player.cards[i] == null) { continue; }
+                if (player.cards[i].Equals(new Card() { Name = "Ace", Value = 11 }))
                 {
                     // Switch the ace to soft.
-                    playerCards[i] = new Card() { Name = "Ace", Value = 1 };
-                    playerTotal += 1;
+                    player.cards[i] = new Card() { Name = "Ace", Value = 1 };
+                    currPlayerTotal += 1;
                     Console.WriteLine("Your card #{0} has been changed from a hard ace to a soft ace.", i);
                     alteredFlag = true;
                 }
                 else
                 {
-                    playerTotal += playerCards[i].Value;
+                    currPlayerTotal += player.cards[i].Value;
                 }
             }
             if (alteredFlag)
             {
-                Console.WriteLine("Your new hand total is: {0}", playerTotal);
+                Console.WriteLine("Your new hand total is: {0}", player.total);
             }
         }
 
-        static void Hit()
+        static void Hit(Player player)
         {
-            playerCardCount += 1;
-            playerCards[playerCardCount] = deck.draw();
-            playerTotal += playerCards[playerCardCount].Value;
-            Console.WriteLine("Your card is a(n) {0} and your new total is {1}. ", playerCards[playerCardCount].Name, playerTotal);
+            player.cardCount += 1;
+            player.cards[player.cardCount] = deck.draw();
+            player.total += player.cards[player.cardCount].Value;
+            Console.WriteLine("Your card is a(n) {0} and your new total is {1}. ", player.cards[player.cardCount].Name, player.total);
 
             //Is this true? I don't think it is.
-            if (playerTotal.Equals(21))
+            if (player.total.Equals(21))
             {
                 Console.WriteLine("You got Blackjack! The dealer's total was {0}. ", dealerTotal);
 
             }
-            else if (playerTotal > 21)
+            else if (player.total > 21)
             {
-                AlterAces();
+                AlterAces(player);
                 // Still check to see if we're over 21.
-                if (playerTotal > 21)
+                if (player.total > 21)
                 {
-                    Console.WriteLine("You busted! Sorry! The dealer's total was {0}", dealerTotal);
+                    Console.WriteLine("You busted! Sorry!");
                     return;
                 }
             }
@@ -167,7 +192,7 @@ namespace BlackjackUpdated
             while (!playerChoice.Equals("H") && !playerChoice.Equals("S"));
             if (playerChoice.ToUpper() == "H")
             {
-                Hit();
+                Hit(player);
             }
         }
 
@@ -187,8 +212,8 @@ namespace BlackjackUpdated
                 Console.ReadLine();
                 Console.Clear();
                 dealerTotal = 0;
-                playerCardCount = 1;
-                playerTotal = 0;
+                // playerCardCount = 1;
+                // playerTotal = 0;
             }
             else if (playAgain.Equals("N"))
             {
