de20dfbf975ec4ac15b1f73b93a725f8b5397627
Tyler Corradi <tyler.corradi@buildertrend.com>
2023-05-31 12:55:23 -0500

    removing old project and moving things to their own class

--- Blackjack/BlackjackUpdated/Program.cs
+++ Blackjack/BlackjackUpdated/Program.cs
@@ -1,5 +1,7 @@
-﻿using System.Runtime.CompilerServices;
+﻿using Blackjack;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using static Blackjack.ConsoleControlHandler;
 
 namespace BlackjackUpdated
 {
@@ -200,44 +202,6 @@ namespace BlackjackUpdated
             }
         }
 
-        private static bool ConsoleCtrlCheck(CtrlTypes ctrlType)
-        {
-            // Put your own handler here
-            switch (ctrlType)
-            {
-                case CtrlTypes.CTRL_C_EVENT:
-                case CtrlTypes.CTRL_BREAK_EVENT:
-                case CtrlTypes.CTRL_CLOSE_EVENT:                    
-                case CtrlTypes.CTRL_LOGOFF_EVENT:
-                case CtrlTypes.CTRL_SHUTDOWN_EVENT:
-                    Environment.Exit(0);
-                    break;
-            }
-            return true;
-        }
-
-        #region unmanaged
-
-        // Declare the SetConsoleCtrlHandler function
-        // as external and receiving a delegate.
-        [DllImport("Kernel32")]
-        public static extern bool SetConsoleCtrlHandler(HandlerRoutine Handler, bool Add);
-
-        // A delegate type to be used as the handler routine
-        // for SetConsoleCtrlHandler.
-        public delegate bool HandlerRoutine(CtrlTypes CtrlType);
-
-        // An enumerated type for the control messages
-        // sent to the handler routine.
-        public enum CtrlTypes
-        {
-            CTRL_C_EVENT = 0,
-            CTRL_BREAK_EVENT,
-            CTRL_CLOSE_EVENT,
-            CTRL_LOGOFF_EVENT = 5,
-            CTRL_SHUTDOWN_EVENT
-        }
-
-        #endregion
+        
     }
 }
