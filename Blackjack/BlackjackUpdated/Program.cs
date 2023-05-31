d23c37aa82c4b428e3d3952d72b3df486ef6fbff
Ally Whitmer <ally.whitmer@buildertrend.com>
2023-05-31 11:46:44 -0500

    moved the Card class to separate file

--- Blackjack/BlackjackUpdated/Program.cs
+++ Blackjack/BlackjackUpdated/Program.cs
@@ -28,8 +28,8 @@
                 {
                     //Currently, just get a value between 16-21 for the dealer
                     dealerTotal = cardRandomizer.Next(15, 22);
-                    playerCards[0] = DealCard();
-                    playerCards[1] = DealCard();
+                    playerCards[0] = Card.DealCard();
+                    playerCards[1] = Card.DealCard();
 
                     playerTotal += playerCards[0].Value;
                     playerTotal += playerCards[1].Value;
@@ -89,7 +89,7 @@
         static void Hit()
         {
             playerCardCount += 1;
-            playerCards[playerCardCount] = DealCard();
+            playerCards[playerCardCount] = Card.DealCard();
             if (playerCards[playerCardCount].Name == "Ace")
             {
                 if(21 - playerTotal < 11)
@@ -131,40 +131,7 @@
         }
 
         //TODO: Move this class to it's own file.
-        private class Card
-        {
-            public int Value;
-            public string Name;
-        }
-
-        static Card DealCard()
-        {
-            int cardValue = cardRandomizer.Next(1, 14);
-            //playerTotal += cardValue;
-            return GetCardValue(cardValue);
-        }
-
-
-        static Card GetCardValue(int cardValue)
-        {
-            return cardValue switch
-            {
-                1 => new Card() { Name = "Two", Value = 2 },
-                2 => new Card() { Name = "Three", Value = 3 },
-                3 => new Card() { Name = "Four", Value = 4 },
-                4 => new Card() { Name = "Five", Value = 5 },
-                5 => new Card() { Name = "Six", Value = 6 },
-                6 => new Card() { Name = "Seven", Value = 7 },
-                7 => new Card() { Name = "Eight", Value = 8 },
-                8 => new Card() { Name = "Nine", Value = 9 },
-                9 => new Card() { Name = "Ten", Value = 10 },
-                10 => new Card() { Name = "Jack", Value = 10 },
-                11 => new Card() { Name = "Queen", Value = 10 },
-                12 => new Card() { Name = "King", Value = 10 },
-                13 => new Card() { Name = "Ace", Value = 11 },
-                _ => new Card() { Name = "Two", Value = 2 },
-            };
-        }
+        
 
         static void PlayAgain()
         {
