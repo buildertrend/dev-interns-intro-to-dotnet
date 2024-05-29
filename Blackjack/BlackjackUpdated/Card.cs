325eb04a7eada8739a0283ee85a0e39edae8bbbe
Oscar Lenkaitis <oscar.lenkaitis@buildertrend.com>
2024-05-29 14:41:02 -0500

    Basic game mechanics. Added a deck and a shuffle of the deck.

---
+++ Blackjack/BlackjackUpdated/Card.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack
+{
+    
+    public class Card
+    {
+        public int Value;
+        public string Name;
+        public string Suit;
+
+
+        public static List<Card> BuildDeck()
+        {
+            List<Card> deck = new List<Card>();
+
+            List<string> suits = new List<string> {"Hearts", "Diamonds", "Clubs", "Spades" };
+            
+
+            List<string> names = new List<string>
+            {
+                "Ace",
+                "Two",
+                "Three",
+                "Four",
+                "Five",
+                "Sixe",
+                "Seven",
+                "Eight",
+                "Nine",
+                "Ten",
+                "Jack",
+                "Queen",
+                "King",
+            };
+            
+            foreach (string suit in suits)
+            {
+                foreach (string name in names)
+                {
+                    Card temp = new Card();
+                    temp.Name = name;
+                    temp.Suit = suit;
+                    temp.Value = GetCardValue(name);
+                    deck.Add(temp);
+                }
+            }
+
+
+            return deck;
+        }
+        static int GetCardValue(string name)
+        {
+            return name switch
+            {
+                "Ace" => 11,
+                "Two" => 2,
+                "Three" => 3,
+                "Four" => 4,
+                "Five" => 5,
+                "Sixe" => 6,
+                "Seven" => 7,
+                "Eight" => 8,
+                "Nine" => 9,
+                "Ten" => 10,
+                "Jack" => 10,
+                "Queen" => 10,
+                "King" => 10,
+            };
+           
+        }
+
+        
+    }
+
+    
+}
--- Blackjack/BlackjackUpdated/Program.cs
+++ Blackjack/BlackjackUpdated/Program.cs
@@ -8,13 +8,17 @@ namespace BlackjackUpdated
     class Program
     {
         static Random cardRandomizer = new Random();
+        
 
-        static readonly Card[] playerCards = new Card[11];
+        //static readonly Card[] playerCards = new Card[11];
+        static readonly List<Card> playerCards = new List<Card>(); 
         static int playerTotal = 0;
         static int playerCardCount = 1;
-        private static readonly Card[] dealerCards = new Card[11];
+        private static readonly List<Card> dealerCards = new List<Card>();
         static int dealerTotal = 0;
-        static int dealerCardCount = 0;
+        static int dealerCardCount = 1;
+        static int dealerMinTotal = 0;
+        
 
         //users to store the player choice (hit or stay)
         static string playerChoice = "";
@@ -22,28 +26,35 @@ namespace BlackjackUpdated
         static string playAgain = "Y";
 
         static void Main(string[] args)
-        {
+        {   Random cardPicker = new Random();
+
+            List<Card> deck = new List<Card>();
             SetConsoleCtrlHandler(new HandlerRoutine(ConsoleCtrlCheck), true);
             while (playAgain.ToUpper() == "Y")
             {
                 //StartGame
                 try
                 {
-                    Console.WriteLine("Welcome to Blackjack - are you ready to play? (Y)esss (N)o");
+                    Console.WriteLine("Welcome to Blackjack - are you ready to play? (Y)es (N)o");
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine("hi");
                 }
                 
+
                 var decision = Console.ReadLine().ToUpper();
 
                 if (decision == "Y")
                 {
+                    deck = Card.BuildDeck();
+                    deck = shuffleDeck(deck);
+                    deck = shuffleDeck(deck);
+                    
                     //Currently, just get a value between 16-21 for the dealer
-                    dealerTotal = cardRandomizer.Next(15, 22);
-                    playerCards[0] = DealCard();
-                    playerCards[1] = DealCard();
+                    //dealerTotal = cardRandomizer.Next(15, 22);
+                    playerCards.Add(DealCard(deck));
+                    playerCards.Add(DealCard(deck));
 
                     playerTotal += playerCards[0].Value;
                     playerTotal += playerCards[1].Value;
@@ -51,29 +62,58 @@ namespace BlackjackUpdated
 
                     //TODO: The dealer is dealt one card face up, one card face down.
                     DisplayWelcomeMessage();
+                    dealerCards.Add(DealCard(deck));
+                    dealerCards.Add(DealCard(deck));
+
+                    dealerTotal += dealerCards[0].Value;
+                    dealerTotal += dealerCards[1].Value;
+                    dealerMinTotal += dealerCards[0].Value;
+                    Console.WriteLine($"Dealer has at least {dealerMinTotal}");
                 }
                 else
                 {
                     Environment.Exit(0);
                 }
 
-                /* START GAME LOOP */
-                do
+                if(playerTotal == 21 && dealerTotal != 21)
                 {
-                    Console.WriteLine("Would you like to (H)it or (S)tay?");
-                    playerChoice = Console.ReadLine().ToUpper();
+                    Console.WriteLine("BLACKJACK!!! congrats you won");
                 }
-                while (!playerChoice.Equals("H") && !playerChoice.Equals("H"));
+                else
+                {
+                    do
+                    {
+
+                        Console.WriteLine("Would you like to (H)it or (S)tay?");
+                        playerChoice = Console.ReadLine().ToUpper();
+
+                    }
+                    while (!playerChoice.Equals("H") && !playerChoice.Equals("S"));
+                }
+                /* START GAME LOOP */
+                
 
                 if (playerChoice.Equals("H"))
                 {
                     //hit will get them a card / check the total and ask for another hit
-                    Hit();
+                    Hit(deck);
+
                 }
 
                 if (playerChoice.Equals("S"))
                 {
-                    if (playerTotal > dealerTotal && playerTotal <= 21)
+                    while(dealerTotal < playerTotal && dealerTotal < 21)
+                    {
+                        dealerCardCount += 1;
+                        dealerCards.Add(DealCard(deck));
+                        dealerTotal += dealerCards[dealerCardCount].Value;
+                        dealerMinTotal += dealerCards[dealerCardCount].Value;
+                        Console.WriteLine($"dealer minimum total is {dealerMinTotal}.");
+                        
+
+                    }
+
+                    if (dealerTotal > 21 && playerTotal <=21)
                     {
                         Console.WriteLine("Congrats! You won the game! The dealer's total is {0} ", dealerTotal);
                     }
@@ -81,6 +121,11 @@ namespace BlackjackUpdated
                     {
                         Console.WriteLine("Sorry, you lost! The dealer's total was {0}", dealerTotal);
                     }
+                    else if (dealerTotal == playerTotal)
+                    {
+                        Console.WriteLine($"Dealer wins with {dealerTotal}");
+                    }
+                    
                 }
 
                 /* END GAME LOOP */
@@ -95,30 +140,27 @@ namespace BlackjackUpdated
         /// </summary>
         private static void DisplayWelcomeMessage()
         {
-            Console.WriteLine("You were dealt the cards : {0} and {1} ", playerCards[0].Name, playerCards[1].Name);
+            Console.WriteLine("You were dealt the cards : {0} {2} and {1} {3} ", playerCards[0].Name, playerCards[1].Name, playerCards[0].Suit, playerCards[1].Suit);
             Console.WriteLine("Your playerTotal is {0} ", playerTotal);
             //TODO: Inform the player the value of the dealer's visible card.
         }
 
-        static void Hit()
+        static void Hit(List<Card> deck)
         {
             playerCardCount += 1;
-            playerCards[playerCardCount] = DealCard();
+            playerCards.Add(DealCard(deck));
             playerTotal += playerCards[playerCardCount].Value;
             Console.WriteLine("You card is a(n) {0} and your new Total is {1}. ", playerCards[playerCardCount].Name, playerTotal);
 
-            //Is this true? I don't think it is.
-            if (playerTotal.Equals(21))
-            {
-                Console.WriteLine("You got Blackjack! The dealer's Total was {0}. ", dealerTotal);
+            
 
-            }
-            else if (playerTotal > 21)
+            
+            if (playerTotal > 21)
             {
                 Console.WriteLine("You busted! Sorry! The dealer's Total was {0}", dealerTotal);
 
             }
-            else if (playerTotal < 21)
+            else if (playerTotal <= 21)
             {
                 do
                 {
@@ -128,46 +170,62 @@ namespace BlackjackUpdated
                 while (!playerChoice.Equals("H") && !playerChoice.Equals("S"));
                 if (playerChoice.ToUpper() == "H")
                 {
-                    Hit();
+                    Hit(deck);
                 }
             }
         }
 
-        //TODO: Move this class to it's own file.
-        private class Card
-        {
-            public int Value;
-            public string Name;
-        }
+        
 
-        static Card DealCard()
+        public static Card DealCard(List<Card> deck)
         {
-            int cardValue = cardRandomizer.Next(1, 14);
-            playerTotal += cardValue;
-            return GetCardValue(cardValue);
-        }
-
-
-        static Card GetCardValue(int cardValue)
-        {
-            return cardValue switch
+            Card temp = new Card();
+            if(deck.Count() > 0)
             {
-                1 => new Card() { Name = "Two", Value = 2 },
-                2 => new Card() { Name = "Three", Value = 3 },
-                3 => new Card() { Name = "Four", Value = 4 },
-                4 => new Card() { Name = "Five", Value = 5 },
-                5 => new Card() { Name = "Six", Value = 6 },
-                6 => new Card() { Name = "Seven", Value = 7 },
-                7 => new Card() { Name = "Eight", Value = 8 },
-                8 => new Card() { Name = "Nine", Value = 9 },
-                9 => new Card() { Name = "Ten", Value = 10 },
-                10 => new Card() { Name = "Jack", Value = 10 },
-                11 => new Card() { Name = "Queen", Value = 10 },
-                12 => new Card() { Name = "King", Value = 10 },
-                13 => new Card() { Name = "Ace", Value = 11 },
-                _ => new Card() { Name = "Two", Value = 2 },
-            };
+                temp = deck[0];
+                deck.Remove(deck[0]);
+            }
+            return temp;
+            //int cardValue = cardRandomizer.Next(1, 14);
+            //return GetCardValue(cardValue);
         }
+        public static List<Card> shuffleDeck(List<Card> original)
+        {
+            List<Card> newDeck = new List<Card>();
+            Random cardPicker = new Random();
+
+            while (original.Count() > 0)
+            {
+                int card = cardPicker.Next(0, original.Count());
+                newDeck.Add(original[card]);
+                original.Remove(original[card]);
+
+            }
+
+            return newDeck;
+        }
+
+
+        //static Card GetCardValue(int cardValue)
+        //{
+        //    return cardValue switch
+        //    {
+        //        1 => new Card() { Name = "Two", Value = 2 },
+        //        2 => new Card() { Name = "Three", Value = 3 },
+        //        3 => new Card() { Name = "Four", Value = 4 },
+        //        4 => new Card() { Name = "Five", Value = 5 },
+        //        5 => new Card() { Name = "Six", Value = 6 },
+        //        6 => new Card() { Name = "Seven", Value = 7 },
+        //        7 => new Card() { Name = "Eight", Value = 8 },
+        //        8 => new Card() { Name = "Nine", Value = 9 },
+        //        9 => new Card() { Name = "Ten", Value = 10 },
+        //        10 => new Card() { Name = "Jack", Value = 10 },
+        //        11 => new Card() { Name = "Queen", Value = 10 },
+        //        12 => new Card() { Name = "King", Value = 10 },
+        //        13 => new Card() { Name = "Ace", Value = 11 },
+        //        _ => new Card() { Name = "Two", Value = 2 },
+        //    };
+        //}
 
         static void PlayAgain()
         {
@@ -186,6 +244,9 @@ namespace BlackjackUpdated
                 dealerTotal = 0;
                 playerCardCount = 1;
                 playerTotal = 0;
+                dealerCardCount = 1;
+                dealerMinTotal = 0;
+                
             }
             else if (playAgain.Equals("N"))
             {
