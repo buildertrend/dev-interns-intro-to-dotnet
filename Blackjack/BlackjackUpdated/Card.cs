7a09f101902496e6125fe9705ff52546cfbd0dea
Tessa Lanzel <tessa.lanzel@buildertrend.com>
2024-05-29 14:33:15 -0500

    Fixed Errors, created a deck of cards and shuffling, started introducing multiplayer game

---
+++ Blackjack/BlackjackUpdated/Card.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack
+{
+    //TODO: Move this class to it's own file.
+    internal class Card
+    {
+        public int Value;
+        public string Name;
+        public string Suit;
+    }
+
+    
+}
