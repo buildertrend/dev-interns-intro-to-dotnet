301bd9283019e75c3c7e4e891d9a37f2baaa7ba5
Alexander Ryan <alexander.ryan@buildertrend.com>
2024-05-29 15:07:05 -0500

    added multiple games back to back

--- Blackjack/BlackjackUpdated/Program.cs
+++ Blackjack/BlackjackUpdated/Program.cs
@@ -7,13 +7,19 @@ namespace BlackjackUpdated
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the number of players:");
-            int numberOfPlayers = int.Parse(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Enter the number of players:");
+                int numberOfPlayers = int.Parse(Console.ReadLine());
 
-            Blackjack game = new Blackjack();
-            game.PlayGame(numberOfPlayers);
+                Blackjack game = new Blackjack();
+                game.PlayGame(numberOfPlayers);
 
-            Console.WriteLine("Game over. Press any key to exit.");
+                Console.WriteLine("Game over. Would you like to play again? (Y)es or (N)o:");
+            }
+            while (Console.ReadLine().ToUpper() == "Y");
+
+            Console.WriteLine("Thanks for playing! Press any key to exit.");
             Console.ReadKey();
         }
     }
